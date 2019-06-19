0482405a558500465a72d4955a6f15bc32d26811
yc <3095220780@qq.com>
2019-06-19 10:03:25 +0800

    ws

--- app/controller/ws.js
+++ app/controller/ws.js
@@ -4,27 +4,30 @@ import { insert, find } from '../db/func/item'
 
 const router = koaRouter()
 
-const ctxArr =[]
+let ctxArr =[]
 
 router.all('/ws',async(ctx)=>{
-    console.log(2)
     let result = await find()
-    ctx.websocket.send(JSON.stringify(ctx.websocket))
-    ctxArr.push({ws:ctx.websocket,user:ctx.cookies.get('UserId')})
+    ctx.websocket.send(JSON.stringify(result))
 
-    for(var i = ctxArr.length;i>=0;i--){
-        if(ctxArr[i].user===ctx.cookies.get('UserId')){
-            ctxArr[i].ws.close()
-            ctxArr.splice(i,1)
-            break
+    ctxArr = [...ctxArr.filter((item)=>{
+        if(item.ws.readyState>1){
+            return false
+        } else if (item.user===ctx.cookies.get('UserId')){
+            item.ws.close()
+            return false
+        } else {
+            return true
         }
-    }
+    })] 
+
+    ctxArr.push({ws:ctx.websocket,user:ctx.cookies.get('UserId')})
 
     ctx.websocket.on('message',async(message)=>{
         await insert(JSON.parse(message))
         let inserted = await find()
         for(let i = 0, len = ctxArr.length; i < len; i++){
-            ctxArr[i].websocket.send(JSON.stringify(inserted))
+            ctxArr[i].ws.send(JSON.stringify(inserted))
         }
     })
 })
